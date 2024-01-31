ac13cc8c93e00472544868ddb9b6534d2f9e0c68
LingKa <cnfty786@gmail.com>
2024-02-01 00:03:43 +0800

    chore: add quick start script
    Signed-off-by: LingKa <cnfty786@gmail.com>

---
+++ script/quick_start.sh
@@ -0,0 +1,5 @@
+go run main.go -name=node1 -addr=127.0.0.1:8081 -config=node1 &
+go run main.go -name=node2 -addr=127.0.0.1:8082 -config=node2 &
+go run main.go -name=node3 -addr=127.0.0.1:8083 -config=node3 &
+go run main.go -name=node4 -addr=127.0.0.1:8084 -config=node4 &
+go run main.go -name=node5 -addr=127.0.0.1:8085 -config=node5 &
